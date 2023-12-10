5cda300d0ec556dcd4d229d3fee3283de8f44930
Duc Khai <khaiducnguyen325@gmail.com>
2023-12-10 22:37:33 +0700

    final

---
+++ app/api/servers/[serverId]/leave/route.ts
@@ -0,0 +1,48 @@
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    const serverId = params.serverId;
+
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (!serverId) {
+      return new NextResponse('Serve ID missing', { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        profileId: {
+          not: profile.id,
+        },
+        members: {
+          some: {
+            profileId: profile.id,
+          },
+        },
+      },
+      data: {
+        members: {
+          deleteMany: {
+            profileId: profile.id,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log(['SERVER_LEAVE_ERROR'], error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
